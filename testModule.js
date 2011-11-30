6d3764787fffffbb677833ac5d692f3d35283922
Egor Egorov <me@egorfine.com>
2011-11-30 12:53:23 +0200

    test

---
+++ testModule.js
@@ -0,0 +1,44 @@
+/**
+
+A test module. 
+
+@param {Integer} one a test parameter with type
+@param two a test parameter with no type
+
+*/
+
+function Test(one, two) {
+}
+
+/**
+
+A test method. 
+
+@return {Integer} return with type
+
+*/
+
+Test.prototype.method = function() {
+}
+
+/** 
+
+A static method, returns type with no description
+
+@return {Boolean} 
+
+*/
+
+Test.staticMethod=function() {
+}
+
+
+/**
+
+@return something
+
+*/
+
+Test.prototype.method2 = function() {
+}
+
